9c5772e294de00b46f64bea9278ff244efd1fbac
alexjova29 <60717092+alexjova29@users.noreply.github.com>
2020-02-10 16:29:11 -0600

    Calificacion

---
+++ calificacion.js
@@ -0,0 +1,14 @@
+var x=Math.floor(Math.random()*10);
+console.log(x);
+if(x==10)
+ console.log("Sacaste: A");
+else if(x==9)
+ console.log("Sacaste: B");
+else if(x==8)
+ console.log("Sacaste: C");
+else if(x==7)
+ console.log("Sacaste: D");
+else if(x==6)
+ console.log("Sacaste: E");
+else
+ console.log("Sacaste: F");
