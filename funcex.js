25e5dd5d03fc159daf315091ca3139cbc1cc3b8f
raghum76 <raghum76@ucol.mx>
2020-02-24 15:23:59 -0600

    Ajuste de sumafin
    Vas llevando tu acumulado y le sumas 1 todo el tiempo, son hacerlo al final es suficiente.

--- funcex.js
+++ funcex.js
@@ -1,14 +1,15 @@
 var n=4;
 function fact(n){
-var fact=1;
-var suma=0;
-var sumafin;
+    var fact=1;     //Todo el codigo de la funcion debe ir adentro, el for al mismo nivel y lo que adentro del for tambien lo identas
+    var suma=0;
+    var sumafin;
     for(var x=1;x<=n;x++){
         fact*=x;
         suma+=Math.pow(n,x)/fact;
-        sumafin=suma+1;
+        
      }
-        return sumafin;
+     sumafin=suma+1;  // Esta instruccion la pudiste haber dejado fuera del ciclo for.
+     return sumafin;
 }console.log(fact(n));
 
 var fact1=1;
